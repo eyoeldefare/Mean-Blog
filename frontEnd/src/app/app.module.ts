0b2230b8fcdd527be554a38e0156c1b9e0660458
eyoeldefare <edefare@masonlive.gmu.edu>
2017-07-26 23:41:13 -0400

    server

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from "@angular/http";
+import { AppRoutingModule } from './app.routing.module'
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PopularComponent } from './popular/popular.component';
+import { RecentComponent } from './recent/recent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    PopularComponent,
+    RecentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontEnd/src/app/app.routing.module.ts
@@ -0,0 +1,41 @@
+import { RouterModule, Routes } from "@angular/router"
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PopularComponent } from './popular/popular.component';
+import { RecentComponent } from './recent/recent.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'popular',
+    component: PopularComponent
+  },
+  {
+    path:'recent',
+    component:RecentComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+
+]
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(appRoutes)],
+  providers: [],
+  bootstrap: [],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
