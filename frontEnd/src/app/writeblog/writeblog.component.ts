4cedf5d9c154cb92b4039c4fdc42e08fef2cdfb8
eyoeldefare <edefare@masonlive.gmu.edu>
2017-09-04 01:41:47 -0400

    newdesign

--- frontEnd/src/app/writeblog/writeblog.component.ts
+++ frontEnd/src/app/writeblog/writeblog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from "../service/blog.service";
 import { ActivatedRoute } from "@angular/router";
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { FormControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 @Component({
   selector: 'app-writeblog',
   templateUrl: './writeblog.component.html',
@@ -8,23 +10,48 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class WriteblogComponent implements OnInit {
   id: any;
-  blog;
+  googledoc: any;
+  title: String;
+  createdBy: String;
+  date;
+  form;
+  newComment = [];
 
-  constructor(private bs: BlogService, private route: ActivatedRoute) { }
+  constructor(private bs: BlogService, private fb: FormBuilder, private route: ActivatedRoute, private sanitizer: DomSanitizer) {
+    this.commentForm();
+  }
 
   ngOnInit() {
     this.getEachblogs()
+
+  }
+  commentForm() {
+    this.form = this.fb.group({
+      comment: ["", Validators.maxLength(800)]
+    })
+  }
+  submitComment(id) {
+    const comment = this.form.get("comment").value;
+    this.bs.comments(id,comment).subscribe(data =>{
+      const index = this.newComment.indexOf(id);
+      
+    })
+  }
+
+  cancel(id) {
+    const index = this.newComment.indexOf(id);
+    this.newComment.slice(index, 1);
+    this.form.reset();
   }
 
   getEachblogs() {
     this.bs.getEachblogs(this.route.snapshot.params['id']).subscribe(data => {
-      this.blog = data.blog;
-      console.log(this.blog.googledoc);
+      this.googledoc = this.sanitizer.bypassSecurityTrustHtml(data.blog.googledoc);
+      this.createdBy = data.blog.createdBy;
+      this.date = data.blog.createdAt;
+      this.title = data.blog.title;
     });
   }
-
-
-
 }
 
 
